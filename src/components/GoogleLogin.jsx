7e6acc55d5ee264885cdf9fbc7ab00f3faf8f891
gunbambam <gunbam715@gmail.com>
2023-04-08 15:35:02 +0900

    refator: 불필요한 코드 삭제, 회원가입 ui 부분 수정

--- src/components/GoogleLogin.jsx
+++ src/components/GoogleLogin.jsx
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import React, { Component, useEffect } from 'react'
-import { Navigate, Route, Redirect, useNavigate, redirect, Link } from 'react-router-dom';
+import React, { Component} from 'react'
 
 export default class GoogleLogin extends Component {
   constructor(props) {
@@ -13,7 +12,7 @@ export default class GoogleLogin extends Component {
   handleLogin = async () => {
     const redirectUrl = encodeURIComponent('http://localhost:3000/SignIn');
     const response = await axios.get(`http://localhost:8080/api/oauth/access?redirectUrl=${redirectUrl}`) // 1. 백엔드 서버에서 Authorization Code 요청 URL 받아오기
-    console.log(response.data.result);
+
     window.location.href = response.data.result; // 구글 회원가입으로 이동
   };
 
@@ -23,7 +22,6 @@ export default class GoogleLogin extends Component {
     const redirectUrl = encodeURIComponent('http://localhost:3000/SignIn'); // Redirect Url -> handleLogin의 redirectUri와 반드시 일치
 
     if (authorizationCode) {
-      console.log('if문은?');
       axios
       .get(`http://localhost:8080/api/oauth/login?authorizationCode=${authorizationCode}&redirectUrl=${redirectUrl}`) // 2. 백엔드 서버와 인증 통신 후 사용자 정보 + 토큰 받아오기
         .then((response) => {
@@ -37,17 +35,14 @@ export default class GoogleLogin extends Component {
         })
         .catch((error) => {
           if (error.response.status === 401) { // DB에 사용저 정보 X -> 회원가입 페이지로 이동
-            console.log(error.response.data);
-            console.log('흠');
-
             localStorage.setItem("name" , error.response.data.name);
             localStorage.setItem("email", error.response.data.email);
+
             window.location.href = 'http://localhost:3000/SignUp';
           }
         });
     }
   };
-
 
   componentDidMount() {
     this.handleCallback();
@@ -68,8 +63,6 @@ export default class GoogleLogin extends Component {
       <div onClick={this.handleLogin}>
         <img src={process.env.PUBLIC_URL + '/img/google_signIn.svg'} />
       </div>
-
-
 
     );
   }

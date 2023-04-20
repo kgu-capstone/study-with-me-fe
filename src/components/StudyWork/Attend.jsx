53e695558d650293ebf71aec892e300f02e4e29b
gunbambam <gunbam715@gmail.com>
2023-04-20 22:22:22 +0900

    feat: 스터디 세부 활동페이지 - 출석부 영역 / 기본 UI구현

--- src/components/StudyWork/Attend.jsx
+++ src/components/StudyWork/Attend.jsx
@@ -5,8 +5,104 @@ export default class Attend extends Component {
   render() {
     return (
           <div className={`${styles.right_container}`}>
-            <div className={`${styles.attend_each_contianer}`}>            
-              Attend
+            <div className={`${styles.attend_contianer}`}>  
+              <div className={`${styles.attend_name_contianer}`}>
+                <div className={`${styles.attend_name_field} ${styles.regular_24}`}>
+                  <p>이름</p>
+                  
+                </div>
+                <div className={`${styles.attend_names} ${styles.regular_16}`}>
+                  <p>닉네임</p>
+                  <p>닉네임</p>
+                  <p>닉네임</p>
+                  <p>닉네임</p>
+                  <p>닉네임</p>
+                  <p>닉네임</p>
+                </div>                
+              </div>          
+
+              {/* 주차별 */}
+              <div className={`${styles.attend_weeks_contianer}`}>
+                <div className={`${styles.attend_each_weeks}`}>
+                  <div className={`${styles.attend_weeks_field} ${styles.regular_24}`}>
+                    <p>2주차</p>
+                    
+                  </div>
+
+                  <div className={`${styles.attend_weeks} ${styles.regular_16}`}>
+                    <p>출석</p>
+                    <p>미출결</p>
+                    <p>출석</p>
+                    <p>결석</p>
+                    <p>지각</p>
+                    <p>-</p>
+                  </div>
+                </div>
+
+                <div className={`${styles.attend_each_weeks}`}>
+                  <div className={`${styles.attend_weeks_field} ${styles.regular_24}`}>
+                    <p>1주차</p>
+                    
+                  </div>
+
+                  <div className={`${styles.attend_weeks} ${styles.regular_16}`}>
+                    <p>출석</p>
+                    <p>미출결</p>
+                    <p>출석</p>
+                    <p>결석</p>
+                    <p>지각</p>
+                    <p>-</p>
+                  </div>
+                </div>
+
+                <div className={`${styles.attend_each_weeks}`}>
+                  <div className={`${styles.attend_weeks_field} ${styles.regular_24}`}>
+                    <p>1주차</p>
+                    
+                  </div>
+
+                  <div className={`${styles.attend_weeks} ${styles.regular_16}`}>
+                    <p>출석</p>
+                    <p>미출결</p>
+                    <p>출석</p>
+                    <p>결석</p>
+                    <p>지각</p>
+                    <p>-</p>
+                  </div>
+                </div>
+                <div className={`${styles.attend_each_weeks}`}>
+                  <div className={`${styles.attend_weeks_field} ${styles.regular_24}`}>
+                    <p>1주차</p>
+                    
+                  </div>
+
+                  <div className={`${styles.attend_weeks} ${styles.regular_16}`}>
+                    <p>출석</p>
+                    <p>미출결</p>
+                    <p>출석</p>
+                    <p>결석</p>
+                    <p>지각</p>
+                    <p>-</p>
+                  </div>
+                </div>
+                <div className={`${styles.attend_each_weeks}`}>
+                  <div className={`${styles.attend_weeks_field} ${styles.regular_24}`}>
+                    <p>1주차</p>
+                    
+                  </div>
+
+                  <div className={`${styles.attend_weeks} ${styles.regular_16}`}>
+                    <p>출석</p>
+                    <p>미출결</p>
+                    <p>출석</p>
+                    <p>결석</p>
+                    <p>지각</p>
+                    <p>-</p>
+                  </div>
+                </div>
+                
+              </div>
+              
             </div>
           </div>
 

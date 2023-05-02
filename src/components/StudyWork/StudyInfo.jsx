02ea792213564334464e8d1885f6d659aaf02ff0
CheonJiyun <70828192+gunbambam@users.noreply.github.com>
2023-05-02 11:23:04 +0900

    [feat] 스터디 활동 페이지의 info영역을 서버와 연결한다.

--- src/components/StudyWork/StudyInfo.jsx
+++ src/components/StudyWork/StudyInfo.jsx
@@ -1,22 +1,48 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '../../css/StudyWork.module.css';
+import { authApi } from '../../services/api';
 
-export default class StudyInfo extends Component {
-  render() {
+export default function StudyInfo({studyId}) {
+
+  // 서버에서 스터디 info 받아오기
+  const [studyThumbnail, setStudyThumbnail] = useState('');
+  const [studyCategory, setStudyCategory] = useState('');
+  const [studyName, setStudyName] = useState('');
+  const [studyCurrentMembers, setStudyCurrentMembers] = useState(0);
+  const [studyMaxMembers, setStudyMaxMembers] = useState(0);
+
+  useEffect(() => {
+    authApi.get(`studies/${studyId}`)
+    .then((response) => {
+      
+      setStudyThumbnail(response.data.thumbnail);
+      setStudyCategory(response.data.category);
+      setStudyName(response.data.name);
+      setStudyCurrentMembers(response.data.currentMembers);
+      setStudyMaxMembers(response.data.maxMembers);
+
+      document.getElementsByClassName(`${styles.info_container}`)[0].style.backgroundColor = `${response.data.thumbnailBackground}`;
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+  })
+
+  
     return (
       <div>
         <div className={styles.info_container}>
           <div className={styles.infoes}>
             <div className={styles.info_left}>
               <div className={styles.info_left_element}>
-                <img src={process.env.PUBLIC_URL + '/img/study_profile.svg'}/>
+                <img src={process.env.PUBLIC_URL + `/img/study_thumbnail/${studyThumbnail}`}/>
               </div>
             </div>
             <div className={styles.info_right}>
               <div className={styles.info_right_element}>
-                <p className={styles.info_category}>#분야</p>
-                <p className={`${styles.bold_24} ${styles.info_studyname}`}>스터디 이름</p>
-                <p className={styles.regular_18}>0 / 10</p>
+                <p className={styles.info_category}>#{studyCategory}</p>
+                <p className={`${styles.bold_24} ${styles.info_studyname}`}>{studyName}</p>
+                <p className={styles.regular_18}>{studyCurrentMembers} / {studyMaxMembers}</p>
               </div>
               <div className={`${styles.info_right_buttons}`}>
                 <div>
@@ -31,5 +57,4 @@ export default class StudyInfo extends Component {
         </div>
       </div>
     )
-  }
 }

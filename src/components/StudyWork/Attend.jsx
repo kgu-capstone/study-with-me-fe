af0f23458b1f4c0ebca97f2fac5896faddd663b6
CheonJiyun <70828192+gunbambam@users.noreply.github.com>
2023-05-03 13:53:35 +0900

    [feat] 출석부 주차의 좌우 스크롤을 구현한다. (#52)
    ㅇㅇ

--- src/components/StudyWork/Attend.jsx
+++ src/components/StudyWork/Attend.jsx
@@ -1,8 +1,53 @@
-import React, { Component } from 'react'
+import React, { useRef, useState } from 'react'
 import styles from '../../css/StudyWork.module.css';
 
-export default class Attend extends Component {
-  render() {
+export default function Attend() {
+
+  const scrollRef = useRef(null);
+  const [isDrag, setIsDrag] = useState(false);
+  const [startX, setStartX] = useState();
+
+  const onDragStart = (e) => {
+    e.preventDefault();
+    setIsDrag(true);
+    setStartX(e.pageX + scrollRef.current.scrollLeft);
+  }
+
+  const onDragEnd = () => {
+    setIsDrag(false);
+  }
+
+  const onDragMove = (e) => {
+    if(isDrag) {
+      const {scrollWidth, clientWidth, scrollLeft} = scrollRef.current;
+
+      scrollRef.current.scrollLeft = startX - e.pageX;
+
+      if(scrollLeft === 0){
+        setStartX(e.pageX);
+      }
+      else if(scrollWidth <= clientWidth + scrollLeft) {
+        setStartX(e.pageX + scrollLeft);
+      }
+    }
+  }
+
+  const throttle = (func, ms) => {
+    let throttle = false;
+    return (...args) => {
+      if( !throttle){
+        throttle = true;
+        setTimeout(() => {
+          func(...args);
+          throttle = false;
+        }, ms);
+      }
+    }
+  }
+
+  const delay = 10;
+  const onThrottleDragMove = throttle(onDragMove, delay);
+
     return (
           <div className={`${styles.right_container}`}>
             <div className={`${styles.attend_contianer}`}>  
@@ -22,7 +67,31 @@ export default class Attend extends Component {
               </div>          
 
               {/* 주차별 */}
-              <div className={`${styles.attend_weeks_contianer}`}>
+              <div className={`${styles.attend_weeks_contianer}`} 
+                ref={scrollRef}
+                onMouseDown={onDragStart}
+                onMouseMove={isDrag ? onThrottleDragMove : null}
+                onMouseUp={onDragEnd}
+                onMouseLeave={onDragEnd}
+                >
+                
+                <div className={`${styles.attend_each_weeks}`}>
+                  <div className={`${styles.attend_weeks_field} ${styles.regular_24}`}>
+                    <p>2주차</p>
+                    
+                  </div>
+
+                  <div className={`${styles.attend_weeks} ${styles.regular_16}`}>
+                    <p>출석</p>
+                    <p>미출결</p>
+                    <p>출석</p>
+                    <p>결석</p>
+                    <p>지각</p>
+                    <p>-</p>
+                  </div>
+                </div>
+
+
                 <div className={`${styles.attend_each_weeks}`}>
                   <div className={`${styles.attend_weeks_field} ${styles.regular_24}`}>
                     <p>2주차</p>
@@ -100,6 +169,55 @@ export default class Attend extends Component {
                     <p>-</p>
                   </div>
                 </div>
+
+                <div className={`${styles.attend_each_weeks}`}>
+                  <div className={`${styles.attend_weeks_field} ${styles.regular_24}`}>
+                    <p>1주차</p>
+                    
+                  </div>
+
+                  <div className={`${styles.attend_weeks} ${styles.regular_16}`}>
+                    <p>출석</p>
+                    <p>미출결</p>
+                    <p>출석</p>
+                    <p>결석</p>
+                    <p>지각</p>
+                    <p>-</p>
+                  </div>
+                </div>
+
+
+                <div className={`${styles.attend_each_weeks}`}>
+                  <div className={`${styles.attend_weeks_field} ${styles.regular_24}`}>
+                    <p>1주차</p>
+                    
+                  </div>
+
+                  <div className={`${styles.attend_weeks} ${styles.regular_16}`}>
+                    <p>출석</p>
+                    <p>미출결</p>
+                    <p>출석</p>
+                    <p>결석</p>
+                    <p>지각</p>
+                    <p>-</p>
+                  </div>
+                </div>
+
+                <div className={`${styles.attend_each_weeks}`}>
+                  <div className={`${styles.attend_weeks_field} ${styles.regular_24}`}>
+                    <p>1주차</p>
+                    
+                  </div>
+
+                  <div className={`${styles.attend_weeks} ${styles.regular_16}`}>
+                    <p>출석</p>
+                    <p>미출결</p>
+                    <p>출석</p>
+                    <p>결석</p>
+                    <p>지각</p>
+                    <p>-</p>
+                  </div>
+                </div>
                 
               </div>
               
@@ -108,5 +226,4 @@ export default class Attend extends Component {
 
 
     )
-  }
 }

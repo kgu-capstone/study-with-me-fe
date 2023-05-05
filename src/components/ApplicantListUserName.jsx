2ccd0246bbd583079427cb9efe2d031d6489793c
CheonJiyun <70828192+gunbambam@users.noreply.github.com>
2023-05-05 09:04:14 +0900

    [feat] 스터디 지원자를 보는 페이지 UI를 구현한다
    [feat] 스터디 지원자를 보는 페이지 UI를 구현한다

---
+++ src/components/ApplicantListUserName.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import styles from '../css/ApplicantListUserName.module.css'
+import Profile from './Profile'
+
+export default function ApplicantListUserName({userId}) {
+
+  //프로필 모달
+  const [isview_profile_modal, setIsview_profile_modal] = useState(false);
+
+
+  return (
+    <div>
+        <div className={styles.userName} onClick={() => setIsview_profile_modal(true)}>
+            <p>{"닉네임"}</p>
+        </div>
+        <div>
+        {isview_profile_modal && <Profile closeModal = {setIsview_profile_modal}/>}
+        </div>
+    </div>
+  )
+}

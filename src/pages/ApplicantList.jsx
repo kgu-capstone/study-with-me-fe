c3c60c9f55f5631deffbfed506a4a6ba851bbe73
CheonJiyun <70828192+gunbambam@users.noreply.github.com>
2023-05-05 09:04:14 +0900

    [feat] 스터디 지원자를 보는 페이지 UI를 구현한다
    [feat] 스터디 지원자를 보는 페이지 UI를 구현한다

--- src/pages/ApplicantList.jsx
+++ src/pages/ApplicantList.jsx
@@ -1,7 +1,71 @@
 import React from 'react'
+import StudyInfo from '../components/StudyWork/StudyInfo'
+import styles from '../css/ApplicantList.module.css'
+import ApplicantListUserName from '../components/ApplicantListUserName'
 
 export default function ApplicantList() {
   return (
-    <div>ApplicantList</div>
+    <div>
+      <StudyInfo />
+      <div className={styles.container}>
+        <div className={styles.child_container}>
+
+          <div>
+          <p className={`${styles.title}`}>스터디 참여자 목록</p>
+          </div>
+
+          <div>
+
+
+            <div className={styles.field}>
+              <div className={styles.field1}>순번</div>
+              <div className={styles.field2}>이름</div>
+              <div className={styles.field3}>참여 결정</div>
+            </div>
+
+
+
+
+            <div className={styles.list}>
+              <div className={styles.data1}>1</div>
+              <div className={styles.data2}><ApplicantListUserName userid={"닉네임"} /></div>
+              <div className={styles.data3}>
+                <div className={styles.approve_button}>
+                  승인
+                </div>
+                <div className={styles.reject_button}>
+                  거절
+                </div>
+              </div>
+            </div>
+
+
+
+            <div className={styles.list}>
+              <div className={styles.data1}>1</div>
+              <div className={styles.data2}><ApplicantListUserName userid={"닉네임"} /></div>
+              <div className={styles.data3}>
+                <div className={styles.approve_button}>
+                  승인
+                </div>
+                <div className={styles.reject_button}>
+                  거절
+                </div>
+              </div>
+            </div>
+
+
+
+          </div>
+
+
+
+
+          
+
+        </div>
+      </div>
+    </div>
+
   )
 }

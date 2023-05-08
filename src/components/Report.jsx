cdb2203c326a9a59585d8b9a94402b3058ddf560
younji66 <youn37@kyonggi.ac.kr>
2023-05-08 15:00:35 +0900

    feat: 신고하기, 프로필보기 구현

--- src/components/Report.jsx
+++ src/components/Report.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, {Component, useEffect, useState} from 'react';
 import styles from '../css/Report.module.css'
+import { authApi } from '../services/api';
+import Avatar from "boring-avatars";
 
-export default function Report({closeModal}) {
+export default function Report({closeModal, reporteeId}) {
+
+  const memberId = localStorage.getItem("id")
+  let [memberNickname, setMemberNickname] = useState('');
+
+  useEffect(() => {
+    authApi.get(`members/${memberId}`)
+    .then((response) => {
+        setMemberNickname(response.data.nickname);
+      })
+      .catch((error) => {
+          console.log(error);
+      })
+  })
+
+  const reportbutton = () => {
+    authApi.post(`members/${reporteeId}`)
+    .then()
+    .catch()
+  }
+
   return (
     <div className={styles.report_container} onClick={() => closeModal(false)}>
       <div className={styles.modal_container} onClick={(e) => e.stopPropagation}>
-      Report
+      
+      <div className={styles.reportq}><b>{memberNickname}님</b>을 신고하시겠습니까?</div>
+      <div className={styles.reason}>사유</div>
+      <div className={styles.square}></div>
+      
+      <div onClick={() => reportbutton()}>
+      <button className={styles.reportbuttonA}>신고하기</button>
       </div>
-    </div>
-  )
+      </div>
+
+      </div>
+    
+  );
 }

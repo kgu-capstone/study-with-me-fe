30e8470f45af8089ad9cfb0e86e695bb2ec62306
CheonJiyun <70828192+gunbambam@users.noreply.github.com>
2023-05-11 14:35:26 +0900

    [feat]스터디 수정 및 종료를 구현한다.
    * feat: 맞춤 UI, 정보 받기 및 수정 api보내기 + refesh Toekn으로 access Token 재발급
    
    * bug: 모집중 숫자로 변경 + 카테고리 숫자 오류 수정 + info영역 사진 경로변경
    
    * feat: 스터디 종료 구현

--- src/services/api.js
+++ src/services/api.js
@@ -11,6 +11,58 @@ const authApi = axios.create({
         headers: {
             Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
         }
-    })
+    });
+
+
+    //refresh token
+    authApi.interceptors.response.use(
+        (response) => {
+            return response
+        },
+
+        async function (error){ 
+            const {
+                config,
+                response : {status},
+            } = error;
+
+
+            const origianlRequest = config;
+
+            if(error.response.status === 401){
+
+                const refreshToken = localStorage.getItem("refreshToken");
+                try{
+                    const {data} = await axios({
+                        method: 'POST',
+                        url: `${process.env.REACT_APP_API_URL}api/token/reissue`,
+                        data: {refreshToken}
+                    })
+
+                    const newAccessToken = data.data.accessToken
+                    const newRefreshToken = data.data.refreshToken
+                    origianlRequest.headers = {
+                        'Content-Type': 'application/json',
+                        Authorization: 'Bearer ' + newAccessToken,
+                    };
+                    localStorage.setItem('accessToken', newAccessToken);
+                    localStorage.setItem('refreshToken', newRefreshToken);
+                    return await axios(origianlRequest);
+
+                }catch(err){
+                    // refresh Token도 만료 -> 로그아웃
+                    localStorage.clear();
+
+                    window.location.href = `${process.env.REACT_APP_BASE_URL}`;
+                }
+            return Promise.reject(error);
+            }
+        }
+        
+    )
+
+
+
+
 
 export { defaultapi, authApi }

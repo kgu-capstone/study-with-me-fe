20b828c04f0dc3e555b80b6ed213e0b5f5577e8b
heemin1008 <125268228+heemin1008@users.noreply.github.com>
2023-05-12 11:38:55 +0900

    added study profile images (#75)

--- src/components/StudyProfileChoice.jsx
+++ src/components/StudyProfileChoice.jsx
@@ -1,47 +1,63 @@
-import React from 'react'
-import styles from '../css/StudyProfileChoice.module.css'
+import React from "react";
+import styles from "../css/StudyProfileChoice.module.css";
 
-export default function StudyProfileChoice({closeModal, setProfile}) {
+export default function StudyProfileChoice({ closeModal, setProfile }) {
+  const profileList = [
+    "certificate_CIP.png",
+    "certificate_EIP.png",
+    "certificate_CSSD.png",
+    "certificate_KH.png",
+    "certificate_GTQ.png",
+    "interview_samsung.png",
+    "interview_hyundai.png",
+    "interview_LG.png",
+    "interview_NCS.png",
+    "language_IELTS.png",
+    "language_JLPT.png",
+    "language_JPT.png",
+    "language_Opic.png",
+    "language_TEPS.png",
+    "language_TOEFL.png",
+    "language_TOEIC.png",
+    "programming_C.png",
+    "programming_Java.png",
+    "programming_JavaScript.png",
+    "programming_Python.png",
+    "programming_Baekjoon.png",
+    "programming_CSS.png",
+    "programming_HTML.png",
+  ];
 
-    const profileList = [
-        'language_001.png',
-        'certifacate_computerLiteracy.png',
-        'etc_teacherExam.png',
-        'interview_samsung.png',
-        'language_toeic.png',
-        'ncs_ncs.png',
-        'programming_baekjoon.png',
-        'certifacate_computerLiteracy.png',
-        'etc_teacherExam.png',
-        'interview_samsung.png',
-        'language_toeic.png',
-        'ncs_ncs.png',
-        'programming_baekjoon.png',
-    ]
-
-
-    const choiceThumbnail = (name) => {
-        setProfile(name);
-        closeModal(false);
-    }
+  const choiceThumbnail = (name) => {
+    setProfile(name);
+    closeModal(false);
+  };
 
   return (
     <div className={styles.profile_container} onClick={() => closeModal(false)}>
-      <div className={styles.modal_container} onClick={(e) => e.stopPropagation()}>
+      <div
+        className={styles.modal_container}
+        onClick={(e) => e.stopPropagation()}
+      >
         <div className={styles.profiles}>
-        {
-            profileList.map((name) => {
-                return(
-                    <>
-                        <div className={styles.each_profile} onClick={() => choiceThumbnail(name)}>
-                            <img width={158} height={158} src={process.env.PUBLIC_URL + `/img/studyprofiles/${name}`}/>
-                        </div>
-                    </>
-                )
-            })
-        }
+          {profileList.map((name) => {
+            return (
+              <>
+                <div
+                  className={styles.each_profile}
+                  onClick={() => choiceThumbnail(name)}
+                >
+                  <img
+                    width={158}
+                    height={158}
+                    src={process.env.PUBLIC_URL + `/img/studyprofiles/${name}`}
+                  />
+                </div>
+              </>
+            );
+          })}
         </div>
       </div>
     </div>
-  )
+  );
 }

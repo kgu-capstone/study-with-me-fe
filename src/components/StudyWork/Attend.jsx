67d5a6176fade395bc7524ad544388602fb9f434
CheonJiyun <70828192+gunbambam@users.noreply.github.com>
2023-05-17 22:34:02 +0900

    [feat] 출석부 api을 연동한다. (#83)
    * feat: 댓글 등록 수정 삭제 구현 + 이미지 사이즈 + refresh Token수정 + 수정페이지onoff버튼오류수정
    
    * [feat] 공지사항 삭제 및 수정 구현 + api연동
    
    * [feat] 출석부 api 연동

--- src/components/StudyWork/Attend.jsx
+++ src/components/StudyWork/Attend.jsx
@@ -1,8 +1,18 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from '../../css/StudyWork.module.css';
 import UserName from '../UserName';
+import { authApi } from '../../services/api';
+import { useLocation } from 'react-router';
 
 export default function Attend() {
+
+  
+  // 넘어올 정보
+  const location = useLocation()
+  const studyId = 1
+  // const studyId = location.state?.studyId
+
+
 
   const scrollRef = useRef(null);
   const [isDrag, setIsDrag] = useState(false);
@@ -49,23 +59,76 @@ export default function Attend() {
   const delay = 10;
   const onThrottleDragMove = throttle(onDragMove, delay);
 
+
+
+
+  const [attendName, setAttendName] = useState([]);
+  const [attendLength, setAttendLength] = useState([]);
+  const [attendInfo, setAttendInfo] = useState({});
+  
+
+  // 출석 정보 조회 api
+  useEffect(() => {
+  
+    authApi.get(`studies/${studyId}/attendances`)
+    .then((response) => {
+      //사용자 이름
+      setAttendName(response.data.summaries["1"])
+
+      //주차 길이 + 주차 정보
+      let lengthtemp = []
+      let infotemp = []
+      for(let i = 1; i <= Object.keys(response.data.summaries).length; i++){
+        lengthtemp.push(i)
+        infotemp.push(response.data.summaries[i])
+      }
+      setAttendLength(lengthtemp);
+      setAttendInfo(infotemp);
+
+
+      console.log(lengthtemp);
+      console.log(infotemp);
+
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+
+  }, [])
+
+
+
+
     return (
           <div className={`${styles.right_container}`}>
             <div className={`${styles.attend_contianer}`}>  
+
+              {/* 우저 이름들 */}
               <div className={`${styles.attend_name_contianer}`}>
                 <div className={`${styles.attend_name_field} ${styles.regular_24}`}>
                   <p>이름</p>
                   
                 </div>
                 <div className={`${styles.attend_names} ${styles.regular_16}`}>
+
+                  {/* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@ api 테스트 전 지워야 함 @@@@@@@@@@@@@@@@@@@@@@@*/}
                   <UserName userNickname={"닉네임"} userId={10} />
-                  <UserName userNickname={"닉네임"} userId={20} />
-                  <UserName userNickname={"닉네임"} userId={30} />
-                  <UserName userNickname={"닉네임"} userId={40} />
-                  <UserName userNickname={"닉네임"} userId={50} />
-                  <UserName userNickname={"닉네임"} userId={60} />
+
+                  {
+                    attendName?attendName.map((item, index) => {
+                      return(
+                        <>
+                          <UserName userNickname={item.participant.nickname} userId={item.participant.id} />
+                        </>
+                      )
+                    })
+                    :
+                    <></>
+                  }
+                  
                 </div>                
               </div>          
+
 
               {/* 주차별 */}
               <div className={`${styles.attend_weeks_contianer}`} 
@@ -74,135 +137,46 @@ export default function Attend() {
                 onMouseMove={isDrag ? onThrottleDragMove : null}
                 onMouseUp={onDragEnd}
                 >
+
+                  {                  
+                    attendLength?attendLength.map((week, index) => {
+                      return(
+                        <>
+                          <div className={`${styles.attend_each_weeks}`}>
+                            <div className={`${styles.attend_weeks_field} ${styles.regular_24}`}>
+                              <p>{week}주차</p>
+                              
+                            </div>
+
+                            <div className={`${styles.attend_weeks} ${styles.regular_16}`}>
+                              {/* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@ api 테스트 전 지워야 함 @@@@@@@@@@@@@@@@@@@@@@@*/}
+                              <p>출석</p>
+
+
+                              {
+                              attendInfo[index].map((attend, attendindex) => {
+                                return(
+                                  <>                                    
+                                    <p>{attend.status}</p>
+                                  </>
+                                )
+                                
+                              })
+                              }
+                              
+                            </div>
+                          </div>
+                        </>
+                      )
+                      
+                    })
+                    :
+                    <>
+                    </>
+                  }
                 
-                <div className={`${styles.attend_each_weeks}`}>
-                  <div className={`${styles.attend_weeks_field} ${styles.regular_24}`}>
-                    <p>2주차</p>
-                    
-                  </div>
 
-                  <div className={`${styles.attend_weeks} ${styles.regular_16}`}>
-                    <p>출석</p>
-                    <p>미출결</p>
-                    <p>출석</p>
-                    <p>결석</p>
-                    <p>지각</p>
-                    <p>-</p>
-                  </div>
-                </div>
-
-
-                <div className={`${styles.attend_each_weeks}`}>
-                  <div className={`${styles.attend_weeks_field} ${styles.regular_24}`}>
-                    <p>2주차</p>
-                    
-                  </div>
-
-                  <div className={`${styles.attend_weeks} ${styles.regular_16}`}>
-                    <p>출석</p>
-                    <p>미출결</p>
-                    <p>출석</p>
-                    <p>결석</p>
-                    <p>지각</p>
-                    <p>-</p>
-                  </div>
-                </div>
-
-                <div className={`${styles.attend_each_weeks}`}>
-                  <div className={`${styles.attend_weeks_field} ${styles.regular_24}`}>
-                    <p>1주차</p>
-                    
-                  </div>
-
-                  <div className={`${styles.attend_weeks} ${styles.regular_16}`}>
-                    <p>출석</p>
-                    <p>미출결</p>
-                    <p>출석</p>
-                    <p>결석</p>
-                    <p>지각</p>
-                    <p>-</p>
-                  </div>
-                </div>
-
-                <div className={`${styles.attend_each_weeks}`}>
-                  <div className={`${styles.attend_weeks_field} ${styles.regular_24}`}>
-                    <p>1주차</p>
-                    
-                  </div>
-
-                  <div className={`${styles.attend_weeks} ${styles.regular_16}`}>
-                    <p>출석</p>
-                    <p>미출결</p>
-                    <p>출석</p>
-                    <p>결석</p>
-                    <p>지각</p>
-                    <p>-</p>
-                  </div>
-                </div>
-                <div className={`${styles.attend_each_weeks}`}>
-                  <div className={`${styles.attend_weeks_field} ${styles.regular_24}`}>
-                    <p>1주차</p>
-                    
-                  </div>
-
-                  <div className={`${styles.attend_weeks} ${styles.regular_16}`}>
-                    <p>출석</p>
-                    <p>미출결</p>
-                    <p>출석</p>
-                    <p>결석</p>
-                    <p>지각</p>
-                    <p>-</p>
-                  </div>
-                </div>
-                <div className={`${styles.attend_each_weeks}`}>
-                  <div className={`${styles.attend_weeks_field} ${styles.regular_24}`}>
-                    <p>1주차</p>
-                    
-                  </div>
-
-                  <div className={`${styles.attend_weeks} ${styles.regular_16}`}>
-                    <p>출석</p>
-                    <p>미출결</p>
-                    <p>출석</p>
-                    <p>결석</p>
-                    <p>지각</p>
-                    <p>-</p>
-                  </div>
-                </div>
-
-                <div className={`${styles.attend_each_weeks}`}>
-                  <div className={`${styles.attend_weeks_field} ${styles.regular_24}`}>
-                    <p>1주차</p>
-                    
-                  </div>
-
-                  <div className={`${styles.attend_weeks} ${styles.regular_16}`}>
-                    <p>출석</p>
-                    <p>미출결</p>
-                    <p>출석</p>
-                    <p>결석</p>
-                    <p>지각</p>
-                    <p>-</p>
-                  </div>
-                </div>
-
-
-                <div className={`${styles.attend_each_weeks}`}>
-                  <div className={`${styles.attend_weeks_field} ${styles.regular_24}`}>
-                    <p>1주차</p>
-                    
-                  </div>
-
-                  <div className={`${styles.attend_weeks} ${styles.regular_16}`}>
-                    <p>출석</p>
-                    <p>미출결</p>
-                    <p>출석</p>
-                    <p>결석</p>
-                    <p>지각</p>
-                    <p>-</p>
-                  </div>
-                </div>
-
+                {/* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@ api 테스트 전 지워야 함 @@@@@@@@@@@@@@@@@@@@@@@*/}
                 <div className={`${styles.attend_each_weeks}`}>
                   <div className={`${styles.attend_weeks_field} ${styles.regular_24}`}>
                     <p>1주차</p>

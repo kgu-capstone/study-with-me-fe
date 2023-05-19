a89debec43b496b9f83256f540fd0f2372fb5a32
gunbambam <gunbam715@gmail.com>
2023-05-19 23:32:07 +0900

    [feat] PEER REVIEW 구현 + 메인페이지 스터디 불러오기 수정

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,6 +3,8 @@ import styles from '../css/Profile.module.css'
 import { authApi } from '../services/api';
 import Avatar from "boring-avatars";
 import ReactApexChart from "react-apexcharts"; 
+import PeerReview from './PeerReview';
+
 
 export default function Profile({closeModal, userId}) {
 
@@ -26,7 +28,16 @@ export default function Profile({closeModal, userId}) {
       .catch((error) => {
           console.log(error);
       })
-  })
+
+      authApi.get(`members/${userId}/reviews`)
+      .then((response) => {
+        setReviews(response.data.reviews);
+      })
+      .catch(e => console.log(e))
+
+
+
+  }, [])
 
   const barData = {
     series: [{
@@ -78,49 +89,87 @@ export default function Profile({closeModal, userId}) {
     },
   }
 
+//PEERVIEW 쓰기
+const [isview_peerriew, setIsView_peerriew] = useState(false);
+
+//PEERVIEW 조회
+const [reviews, setReviews] = useState([]);
 
   return (
     
     <div className={styles.profile_container} onClick={() => closeModal(false)}>
       <div className={styles.modal_container} onClick={(e) => e.stopPropagation()}>
+      
       <div className={styles.nicknameProfile}>{memberNickname}님 프로필</div>
-    <div className={styles.iprof}>
-    <Avatar
-                    size={158}
-                    name="nick"
-                    variant="beam"
-                    colors={["#FF3D1F", "#FFEA52", "#FF5037", "#1FFF98", "#4D2BFF"]}
-                 />      
-    </div>
-    <div className={styles.labelA}>닉네임</div>
-    <div className={styles.labelB}>나이</div>
-    <div className={styles.labelC}>성별</div>
-    <div className={styles.labelD}>지역</div>
-    <div className={styles.labelE}>관심사</div>
+      <div className={styles.iprof}>
+      <Avatar
+                      size={158}
+                      name="nick"
+                      variant="beam"
+                      colors={["#FF3D1F", "#FFEA52", "#FF5037", "#1FFF98", "#4D2BFF"]}
+                  />      
+      </div>
+      <div className={styles.labelA}>닉네임</div>
+      <div className={styles.labelB}>나이</div>
+      <div className={styles.labelC}>성별</div>
+      <div className={styles.labelD}>지역</div>
+      <div className={styles.labelE}>관심사</div>
 
-    <div className={styles.Alabele}>{memberNickname}</div>
-    <div className={styles.Blabele}>{memberBirth}</div>
-    <div className={styles.Clabele}>{memberGender}</div>
-    <div className={styles.Dlabele}>{memberRegion.province} {memberRegion.city}</div>
-    <div className={styles.Elabele}>{memberInterest}</div>
-    
-    <div className={styles.squareA}></div>
-    <div className={styles.astudy}>활동중인 스터디</div>
-    <div className={styles.squareB}></div>
-    <div className={styles.bstudy}>20자 리뷰</div>
+      <div className={styles.Alabele}>{memberNickname}</div>
+      <div className={styles.Blabele}>{memberBirth}</div>
+      <div className={styles.Clabele}>{memberGender}</div>
+      <div className={styles.Dlabele}>{memberRegion.province} {memberRegion.city}</div>
+      <div className={styles.Elabele}>{memberInterest}</div>
+      
+      <div className={styles.squareA}>
+        
+      </div>
+      <div className={styles.astudy}>활동중인 스터디</div>
+      <div className={styles.squareB}>
+        <div className={styles.squareB_top}>
+          <div className={styles.bstudy}>PEER REVIEW</div>
+          <div className={styles.write_review}
+          onClick={() => setIsView_peerriew(true)}
+          >리뷰 쓰기
+          </div>
 
-    <div>
-    <div id="chart"><div className={styles.graph}> 
-        <ReactApexChart 
-            options={barData.options}
-            series={barData.series}
-            type="bar" 
-            
-        />
+
+          
+        </div>
+        
+
+
+        <div className={styles.peer_reivew_content_container}>
+          {reviews.map((item)=>{
+            return(
+              <><div className={styles.peer_review_content} >{item}</div></>
+            )
+          })}
+          
+        </div>
+      </div>
+      
+      <div>
+        
+      </div>
+      
+      <div>
+      <div id="chart"><div className={styles.graph}> 
+          <ReactApexChart 
+              options={barData.options}
+              series={barData.series}
+              type="bar" 
+              
+          />
+      </div>
+      
     </div>
-</div>
-</div>
     </div>
+          <div>
+            {isview_peerriew && <PeerReview closeModal={setIsView_peerriew} userId={userId} userName={memberNickname}/>}
+          </div>
+    </div>
+          
     </div>
     
     

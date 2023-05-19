32a87087ccd8f76f9405b514c2324d51f6cc1af3
heemin1008 <125268228+heemin1008@users.noreply.github.com>
2023-05-19 15:21:53 +0900

    Feature/#84 메인페이지 전체적으로 기능 구현하다. (#93)
    * added study profile images
    
    * feat:81 added banner images
    
    * feat:81 solved error
    
    * feat:85 dropdown sort
    
    * feat:84 added a lot of features

--- src/components/DropDownSort.js
+++ src/components/DropDownSort.js
@@ -1,9 +1,32 @@
 import React from "react";
 import { useState } from "react";
 import { Dropdown } from "react-bootstrap";
+import { authApi } from "../services/api";
+import * as sortManage from "../sortManage";
 
 const DropDownSort = (props) => {
   const [selectedOption, setSelectedOption] = useState(props.title);
+
+
+  const handleOptionSelect = (option, sort) => {
+    props.setRecru_Sort(sort);
+    if (props.title == "모집 최신순") {
+      sortManage.sortManage(
+        props.save_Category_Status,
+        sort,
+        props.save_Sort_Status
+      );
+    } else {
+      sortManage.sortManage(
+        props.save_Category_Status,
+        props.save_Sort_Status,
+        sort
+      );
+    }
+
+    setSelectedOption(option);
+    props.setRecruitStatus(sort);
+
   const handleOptionSelect = (option) => {
     setSelectedOption(option);
     console.log("선택된 값:", option);
@@ -27,7 +50,11 @@ const DropDownSort = (props) => {
               <a
                 class="dropdown-item"
                 href="#"
-                onClick={() => handleOptionSelect(props.sub1)}
+
+                onClick={() => handleOptionSelect(props.sub1, props.sub1_API)}
+
+            
+
               >
                 {props.sub1}
               </a>
@@ -36,7 +63,11 @@ const DropDownSort = (props) => {
               <a
                 class="dropdown-item"
                 href="#"
-                onClick={() => handleOptionSelect(props.sub2)}
+
+                onClick={() => handleOptionSelect(props.sub2, props.sub2_API)}
+
+              
+
               >
                 {props.sub2}
               </a>
@@ -45,7 +76,11 @@ const DropDownSort = (props) => {
               <a
                 class="dropdown-item"
                 href="#"
-                onClick={() => handleOptionSelect(props.sub3)}
+
+                onClick={() => handleOptionSelect(props.sub3, props.sub3_API)}
+
+       
+
               >
                 {props.sub3}
               </a>
